5cfeb03df5cf0ed60ce6b39958b855d3a104cf14
백상휘 <sanghwiback@gmail.com>
2026-04-08 08:47:17 +0900

    [KMP] Converting KMP - 5
    - Add Constraint on PlatformParcelable.ios.kt

--- shared/src/iosMain/kotlin/com/example/twoeyesproject/platformspecific/PlatformParcelable.ios.kt
+++ shared/src/iosMain/kotlin/com/example/twoeyesproject/platformspecific/PlatformParcelable.ios.kt
@@ -1,4 +1,7 @@
 package com.example.twoeyesproject.platformspecific
 
 actual interface PlatformParcelable
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.BINARY)
 actual annotation class Parcelize actual constructor()
