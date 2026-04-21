ea378edeb517d02a1cb789171af31f88afc3913e
백상휘 <sanghwiback@gmail.com>
2026-04-21 16:21:45 +0900

    [KMP] Converting KMP - 12
    - Add database builder

--- shared/src/commonMain/kotlin/com/example/twoeyesproject/dependency/Database.kt
+++ shared/src/commonMain/kotlin/com/example/twoeyesproject/dependency/Database.kt
@@ -4,6 +4,9 @@ import androidx.room.ConstructedBy
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.RoomDatabaseConstructor
+import androidx.sqlite.driver.bundled.BundledSQLiteDriver
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
 
 @Database(entities = [MergeResultEntity::class], version = 1)
 @ConstructedBy(AppDatabaseConstructor::class)
@@ -15,4 +18,13 @@ abstract class AppDatabase : RoomDatabase() {
 @Suppress("KotlinNoActualForExpect")
 expect object AppDatabaseConstructor : RoomDatabaseConstructor<AppDatabase> {
     override fun initialize(): AppDatabase
+}
+
+fun getRoomDatabase(
+    builder: RoomDatabase.Builder<AppDatabase>
+): AppDatabase {
+    return builder
+        .setDriver(BundledSQLiteDriver())
+        .setQueryCoroutineContext(Dispatchers.IO)
+        .build()
 }
