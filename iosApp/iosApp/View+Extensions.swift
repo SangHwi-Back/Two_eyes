ec43f3e388cf0b380e3b47e1a8c3ac0cea05c17e
백상휘 <sanghwiback@gmail.com>
2026-06-29 08:53:49 +0900

    [iOS] AppConstant file issue resolved

--- iosApp/iosApp/View+Extensions.swift
+++ iosApp/iosApp/View+Extensions.swift
@@ -5,4 +5,23 @@
 //  Created by 백상휘 on 6/26/26.
 //
 
-import Foundation
+import Shared
+
+var thumbnailWidth: CGFloat {
+    CGFloat(AppConstants.shared.THUMBNAIL_SIZE_WIDTH)
+}
+var thumbnailHeight: CGFloat {
+    CGFloat(AppConstants.shared.THUMBNAIL_SIZE_HEIGHT)
+}
+var thumbnailSize: CGSize {
+    CGSize(width: thumbnailWidth, height: thumbnailHeight)
+}
+var iconWidth: CGFloat {
+    CGFloat(AppConstants.shared.ICON_SIZE_WIDTH)
+}
+var iconHeight: CGFloat {
+    CGFloat(AppConstants.shared.ICON_SIZE_HEIGHT)
+}
+var iconSize: CGSize {
+    CGSize(width: iconWidth, height: iconHeight)
+}
